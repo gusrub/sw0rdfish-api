1795eb98c26ad53563403fc1f02f0338104378f5
Gustavo Rubio <gus@ahivamos.net>
2018-03-26 00:11:44 -0700

    Added deployment script and config to autodeploy on bitbucket.

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+LONG_COMMIT=$BITBUCKET_COMMIT
+SHORT_COMMIT=${LONG_COMMIT:0:8}
+
+if [[ ! -z "$BITBUCKET_BRANCH" ]]; then
+        if [[ $BITBUCKET_BRANCH == "master" ]]; then
+                echo "Will deploy commit '$SHORT_COMMIT' from $BITBUCKET_BRANCH branch to PRODUCTION environment ($HEROKU_APP_NAME_PROD) in heroku..."
+                sleep 3
+        elif [[ $BITBUCKET_BRANCH == "staging" ]]; then
+                echo "Will deploy commit '$SHORT_COMMIT' from $BITBUCKET_BRANCH branch to STAGING environment ($HEROKU_APP_NAME_STAGING) in heroku..."
+                sleep 3
+        else
+                echo "Will not deploy this commit ($SHORT_COMMIT) from branch '$BITBUCKET_BRANCH' since its not a tracked branch."
+        fi
+else
+        echo "ENV var 'BITBUCKET_BRANCH' is not set! make sure it is for deployment to work."
+        exit 1
+fi
