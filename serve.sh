ce5e5e1cb0a4dabbf5eeae961619e6e8f29e2a22
Gustavo Rubio <gus@ahivamos.net>
2018-09-04 20:17:42 -0700

    Added parameters options to serve script

--- serve.sh
+++ serve.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
 export SW0RDFISH_ENV=development
-php -S localhost:8080 -t public
 
+BIND_PORT=80
+BIND_ADDRESS="0.0.0.0"
+ROOT_FOLDER="public"
+
+while getopts ":a:p:f:" opt; do
+    case "${opt}" in
+        a)
+            BIND_ADDRESS=${OPTARG}
+            ;;
+        p)
+            BIND_PORT=${OPTARG}
+            ;;
+        f)
+            ROOT_FOLDER=${OPTARG}
+            ;;
+        *)
+            printf "\nInvalid option. Options are: -a [bind_address] -p [bind_port]  -f [root_folder]\n\n"
+            exit 1
+            ;;
+    esac
+done
+
+php -S "$BIND_ADDRESS:$BIND_PORT" -t $ROOT_FOLDER
